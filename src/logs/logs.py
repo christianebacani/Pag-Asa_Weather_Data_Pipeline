d23565e54edad03241085bee4777ccabfdbffcb9
christianebacani <christianbacani581@gmail.com>
2025-09-17 16:36:49 +0800

    feat: Added logs

---
+++ src/logs/logs.py
@@ -0,0 +1,9 @@
+'''
+    Data Pipeline Logs
+'''
+def generate_logs_from_job(job: str) -> None:
+    '''
+        Generate function to generate logs
+        when executing different jobs of
+        the pipeline
+    '''
