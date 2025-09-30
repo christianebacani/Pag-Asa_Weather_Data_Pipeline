3f67c4146576fa220fd19b8d582545de6be78314
christianebacani <christianbacani581@gmail.com>
2025-09-30 23:00:52 +0800

    feat: Added new function to import and execute 'scrape_flood_information_data'

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -6,12 +6,13 @@ import os
 sys.path.append(os.path.abspath('src'))
 import pandas as pd
 from datetime import datetime
-from ingest.ingest import scrape_daily_weather_forecast_data
-from ingest.ingest import scrape_weather_outlook_for_selected_ph_cities_data
-from ingest.ingest import scrape_asian_cities_weather_forecast_data
-from ingest.ingest import scrape_weather_outlook_for_selected_tourist_areas_data
-from ingest.ingest import scrape_weekly_weather_outlook_data
-from ingest.ingest import scrape_daily_temperature_data
+from ingest.ingest_weather_data import scrape_daily_weather_forecast_data
+from ingest.ingest_weather_data import scrape_weather_outlook_for_selected_ph_cities_data
+from ingest.ingest_weather_data import scrape_asian_cities_weather_forecast_data
+from ingest.ingest_weather_data import scrape_weather_outlook_for_selected_tourist_areas_data
+from ingest.ingest_weather_data import scrape_weekly_weather_outlook_data
+from ingest.ingest_weather_data import scrape_daily_temperature_data
+from ingest.ingest_flood_data import scrape_flood_information_data
 
 def generate_logs_from_pipeline_job(job: str) -> None:
     '''
@@ -35,4 +36,5 @@ if __name__ == '__main__':
     # asian_cities_weather_forecast = scrape_asian_cities_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather/weather-asian-cities-weather-forecast')
     # weather_outlook_for_selected_tourist_areas = scrape_weather_outlook_for_selected_tourist_areas_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas')
     # weekly_weather_outlook = scrape_weekly_weather_outlook_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-weekly')
-    scrape_daily_temperature_data('https://www.pagasa.dost.gov.ph/weather/low-high-temperature')
+    # daily_temperature_data = scrape_daily_temperature_data('https://www.pagasa.dost.gov.ph/weather/low-high-temperature')
+    scrape_flood_information_data('https://www.pagasa.dost.gov.ph/flood#flood-information')
