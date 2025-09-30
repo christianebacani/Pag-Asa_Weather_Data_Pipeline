ff922873ac870ccd7d566ab3cb2935bab31c2895
christianebacani <christianbacani581@gmail.com>
2025-09-30 22:59:17 +0800

    feat: Added new module

---
+++ src/ingest/ingest_flood_data.py
@@ -0,0 +1,9 @@
+'''
+    Data Ingestion for Flood Data
+'''
+def scrape_flood_information_data(url: str) -> None | dict:
+    '''
+        Scrape function to perform web-scraping
+        to ingest flood information data from
+        the Website of Pag-Asa (https://www.pagasa.dost.gov.ph/)
+    '''
