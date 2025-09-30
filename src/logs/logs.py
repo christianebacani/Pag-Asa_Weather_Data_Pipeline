16408161a0a9af180fb3ff6ae49f5cf405221c01
christianebacani <christianbacani581@gmail.com>
2025-09-30 12:36:50 +0800

    feat: Added new function to execute

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -10,6 +10,8 @@ from ingest.ingest import scrape_daily_weather_forecast_data
 from ingest.ingest import scrape_weather_outlook_for_selected_ph_cities_data
 from ingest.ingest import scrape_asian_cities_weather_forecast_data
 from ingest.ingest import scrape_weather_outlook_for_selected_tourist_areas_data
+from ingest.ingest import scrape_weekly_weather_outlook_data
+from ingest.ingest import scrape_daily_temperature_data
 
 def generate_logs_from_pipeline_job(job: str) -> None:
     '''
@@ -27,7 +29,10 @@ def generate_logs_from_pipeline_job(job: str) -> None:
     logs.to_csv('src/logs/logs.csv', index=False)
 
 if __name__ == '__main__':
+    # Weather Data
     # daily_weather_forecast = scrape_daily_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
     # weather_outlook_for_selected_ph_cities = scrape_weather_outlook_for_selected_ph_cities_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities')
     # asian_cities_weather_forecast = scrape_asian_cities_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather/weather-asian-cities-weather-forecast')
-    scrape_weather_outlook_for_selected_tourist_areas_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas')
+    # weather_outlook_for_selected_tourist_areas = scrape_weather_outlook_for_selected_tourist_areas_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas')
+    # weekly_weather_outlook = scrape_weekly_weather_outlook_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-weekly')
+    scrape_daily_temperature_data('https://www.pagasa.dost.gov.ph/weather/low-high-temperature')    
