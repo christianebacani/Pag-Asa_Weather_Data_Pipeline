9f8278ed05b592e6b8db30168f2f6bfce34f905d
christianebacani <christianbacani581@gmail.com>
2025-10-01 16:29:08 +0800

    feat: Added new content to the function 'scrape_flood_information_data'

--- src/ingest/ingest_flood_data.py
+++ src/ingest/ingest_flood_data.py
@@ -1,9 +1,32 @@
 '''
     Data Ingestion for Flood Data
 '''
+import requests
+from bs4 import BeautifulSoup
+
 def scrape_flood_information_data(url: str) -> None | dict:
     '''
         Scrape function to perform web-scraping
         to ingest flood information data from
         the Website of Pag-Asa (https://www.pagasa.dost.gov.ph/)
     '''
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print(f'Status code: {response.status_code}')
+        print(f'The website didn\'t accept the request!')
+        return None
+    
+    soup = BeautifulSoup(response.text, 'html.parser') # Parse response to a Beautiful Soup object
+    row_flood_page = soup.find('div', attrs={'class': 'row flood-page'})
+    
+    # Scrape basic hydrological forecast
+    article_content = row_flood_page.find('div', attrs={'class': 'col-md-12 article-content'})
+    table = article_content.find('table', attrs={'class': 'table'})
+    list_of_tbody_tags = table.find_all('tbody')
+
+    major_river_basins_table = list_of_tbody_tags[0]
+    table_rows = major_river_basins_table.find_all('tr')
+
+    for table_row in table_rows:
+        print(table_row)
