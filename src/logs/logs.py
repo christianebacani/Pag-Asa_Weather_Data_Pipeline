e016b9ad0bb56676da2d6efcbdce71588f37624c
christianebacani <christianbacani581@gmail.com>
2025-10-03 00:08:39 +0800

    refact: Convert the functions for scraping weather and flood information data to proper comment style

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -31,16 +31,18 @@ def generate_logs_from_pipeline_job(job: str) -> None:
     logs.to_csv('src/logs/logs.csv', index=False)
 
 if __name__ == '__main__':
+    '''
     # Weather Data
-    # daily_weather_forecast = scrape_daily_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
-    # weather_outlook_for_selected_ph_cities = scrape_weather_outlook_for_selected_ph_cities_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities')
-    # asian_cities_weather_forecast = scrape_asian_cities_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather/weather-asian-cities-weather-forecast')
-    # weather_outlook_for_selected_tourist_areas = scrape_weather_outlook_for_selected_tourist_areas_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas')
-    # weekly_weather_outlook = scrape_weekly_weather_outlook_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-weekly')
-    # daily_temperature_data = scrape_daily_temperature_data('https://www.pagasa.dost.gov.ph/weather/low-high-temperature')
+    daily_weather_forecast_data = scrape_daily_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
+    weather_outlook_for_selected_ph_cities_data = scrape_weather_outlook_for_selected_ph_cities_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities')
+    asian_cities_weather_forecast_data = scrape_asian_cities_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather/weather-asian-cities-weather-forecast')
+    weather_outlook_for_selected_tourist_areas_data = scrape_weather_outlook_for_selected_tourist_areas_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas')
+    weekly_weather_outlook_data = scrape_weekly_weather_outlook_data('https://www.pagasa.dost.gov.ph/weather/weather-outlook-weekly')
+    daily_temperature_data = scrape_daily_temperature_data('https://www.pagasa.dost.gov.ph/weather/low-high-temperature')
     
     # Flood data
-    # flood_information_data = scrape_flood_information_data('https://www.pagasa.dost.gov.ph/flood#flood-information')
+    flood_information_data = scrape_flood_information_data('https://www.pagasa.dost.gov.ph/flood#flood-information')
+    '''
 
     # Tropical cyclone data
     scrape_tropical_cyclone_bulletin('https://www.pagasa.dost.gov.ph/tropical-cyclone/severe-weather-bulletin')
