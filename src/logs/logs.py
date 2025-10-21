859995bba1653713f8768c6f4bc84819974b81a1
christianebacani <christianbacani581@gmail.com>
2025-10-22 01:00:21 +0800

    refact: Uncomment only necessary functions to execute for development purposes

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -51,4 +51,4 @@ if __name__ == '__main__':
     # tropical_cyclone_bulletin_data = scrape_tropical_cyclone_bulletin_data('https://www.pagasa.dost.gov.ph/tropical-cyclone/severe-weather-bulletin')
     # tropical_cyclone_warning_for_shipping_data = scrape_tropical_cyclone_warning_for_shipping_data('https://www.pagasa.dost.gov.ph/tropical-cyclone/tropical-cyclone-warning-for-shipping')
     # forecast_storm_surge_data = scrape_forecast_storm_surge_data('https://www.pagasa.dost.gov.ph/tropical-cyclone/forecast-storm-surge')
-    scrape_tropical_cyclone_warning_for_agriculture_data('https://www.pagasa.dost.gov.ph/tropical-cyclone/tropical-cyclone-warning-for-agriculture')
+    # tropical_cyclone_warning_for_agriculture_data = scrape_tropical_cyclone_warning_for_agriculture_data('https://www.pagasa.dost.gov.ph/tropical-cyclone/tropical-cyclone-warning-for-agriculture')
