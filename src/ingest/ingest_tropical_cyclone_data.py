2e775e89d55802237f53d8ba3b1b773c01803c48
christianebacani <christianbacani581@gmail.com>
2025-10-24 00:27:41 +0800

    refact: Modify the comment overview of every function

--- src/ingest/ingest_tropical_cyclone_data.py
+++ src/ingest/ingest_tropical_cyclone_data.py
@@ -6,9 +6,7 @@ from bs4 import BeautifulSoup
 
 def scrape_tropical_cyclone_bulletin_data(url: str) -> dict:
     '''
-        Performs web scraping on the Pag-asa website to
-        ingest tropical cyclone bulletin data. The function
-        retrieves the data containing tropical cyclone bulletin.
+        The function retrieves the data containing tropical cyclone bulletin.
     '''
     response = requests.get(url)
     
@@ -245,9 +243,7 @@ def scrape_tropical_cyclone_bulletin_data(url: str) -> dict:
 
 def scrape_tropical_cyclone_warning_for_shipping_data(url: str) -> dict:
     '''
-        Performs web scraping on the Pag-asa website to
-        ingest tropical cyclone warning for shipping data. The function
-        retrieves the data containing tropical cyclone warning for shipping.
+        The function retrieves the data containing tropical cyclone warning for shipping.
     '''
     response = requests.get(url)
 
@@ -280,19 +276,15 @@ def scrape_tropical_cyclone_warning_for_shipping_data(url: str) -> dict:
 
 def scrape_forecast_storm_surge_data(url: str) -> dict:
     '''
-        Performs web scraping on the Pag-asa website to
-        ingest forecast storm surge data. The function 
-        retrieves the URL containing the forecasted storm 
-        surge data in PDF Format.
+        The function retrieves the URL containing the 
+        forecasted storm surge data in PDF Format.
     '''
     # TODO: To be implemented
     return {}
 
 def scrape_tropical_cyclone_warning_for_agriculture_data(url: str) -> dict:
     '''
-        Performs web scraping on the Pag-asa website to
-        ingest tropical cyclone warning for agriculture
-        data. The function retrieves the data containing
+        The function retrieves the data containing
         tropical cyclone warning for agriculture.
     '''
     response = requests.get(url)
