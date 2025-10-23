9236fcf37e8ac490559c5b645f0db741a0ce4d88
christianebacani <christianbacani581@gmail.com>
2025-10-24 00:26:15 +0800

    refact: Modify the comment overview of every function

--- src/ingest/ingest_flood_data.py
+++ src/ingest/ingest_flood_data.py
@@ -6,9 +6,7 @@ from bs4 import BeautifulSoup
 
 def scrape_flood_information_data(url: str) -> dict:
     '''
-        Performs web scraping on the Pag-asa website to
-        ingest flood information data. The function 
-        retrieves the data containing the flood information.
+        The function retrieves the data containing the flood information.
     '''
     response = requests.get(url)
 
