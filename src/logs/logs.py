ea03dd5f9edac96ec0c263c7ebdddd632ba63fc0
christianebacani <christianbacani581@gmail.com>
2025-11-10 00:02:48 +0800

    feat: Added new content to the function 'sgenerate_logs_from_pipeline_job' to store the logs in the logs dataset

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -1,6 +1,8 @@
 '''
     Data Pipeline Logs
 '''
+import pandas as pd
+
 from datetime import datetime
 
 def generate_logs_from_pipeline_job(job: str) -> None:
@@ -12,3 +14,7 @@ def generate_logs_from_pipeline_job(job: str) -> None:
     format = '%Y-%m-%d %H:%M:%s'
     now = datetime.now()
     timestamp = now.strftime(format)
+
+    logs = pd.read_csv('src/logs/logs.csv')
+    logs = pd.concat([logs, pd.DataFrame({'jobs': [job], 'timestamps': [timestamp]})], ignore_index=True)
+    logs.to_csv('src/logs/logs.csv', index=False)
