56df16b353c8be21c04ad5c26d43ced0fbda2d17
christianebacani <christianbacani581@gmail.com>
2025-11-10 20:29:41 +0800

    refact: Modify the content of the module to fix the parameter name and function name from the module

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -2,19 +2,20 @@
     Data Pipeline Logs
 '''
 import pandas as pd
-
 from datetime import datetime
 
-def generate_logs_from_pipeline_job(job: str) -> None:
+def generate_logs(log_message: str) -> None:
     '''
-        Generate function to generate logs
-        when executing different jobs of
-        the pipeline
+        Function to generate logs based on the ETL Pipeline job/s.
     '''
-    format = '%Y-%m-%d %H:%M:%s'
+    format = '%Y-%m-%d %H:%M:%S'
     now = datetime.now()
     timestamp = now.strftime(format)
-
+    
+    # Generate logs usins pandas for logs dataset (csv format)
     logs = pd.read_csv('src/logs/logs.csv')
-    logs = pd.concat([logs, pd.DataFrame({'jobs': [job], 'timestamps': [timestamp]})], ignore_index=True)
+    logs = pd.concat([logs, pd.DataFrame({'messages': [log_message], 'timestamps': [timestamp]})], ignore_index=True)
     logs.to_csv('src/logs/logs.csv', index=False)
+
+if __name__ == '__main__':
+    generate_logs('START')
