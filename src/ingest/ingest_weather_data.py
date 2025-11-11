56be636ed27cf2064c0eb73d48ac9b657eeb8ac7
christianebacani <christianbacani581@gmail.com>
2025-11-11 17:13:53 +0800

    feat: Added new content to the module to create a function 'ingest_daily_weather_forecast_data'

--- src/ingest/ingest_weather_data.py
+++ src/ingest/ingest_weather_data.py
@@ -2,8 +2,14 @@
     Data Ingestion Module to ingest
     Daily Weather Forecast.
 '''
+import requests
+
 def ingest_daily_weather_forecast_data(url: str) -> dict:
     '''
         Function to ingest daily weather forecast data.
     '''
-    print('Hello World!')
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print('The website does not allow to be scraped!')
+        return {}
