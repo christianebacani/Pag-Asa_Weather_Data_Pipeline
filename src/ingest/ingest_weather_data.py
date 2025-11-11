01aedc3c675054c49f1f55b6f38f4b55fe2f1dac
christianebacani <christianbacani581@gmail.com>
2025-11-11 21:51:43 +0800

    feat: Added new content to the function 'ingest_daily_weather_forecast_data'

--- src/ingest/ingest_weather_data.py
+++ src/ingest/ingest_weather_data.py
@@ -3,6 +3,7 @@
     Daily Weather Forecast.
 '''
 import requests
+from bs4 import BeautifulSoup
 
 def ingest_daily_weather_forecast_data(url: str) -> dict:
     '''
@@ -13,3 +14,7 @@ def ingest_daily_weather_forecast_data(url: str) -> dict:
     if response.status_code != 200:
         print('The website does not allow to be scraped!')
         return {}
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    div_tag_with_row_weather_page_class = soup.find('div', attrs={'class': 'row weather-page'})
+    div_tag_with_row_class = div_tag_with_row_weather_page_class.find('div', attrs={'class': 'row'})
