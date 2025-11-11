b2eb85fc9fea9e1ed1913a4bd13f7f3830a7e385
christianebacani <christianbacani581@gmail.com>
2025-11-11 16:28:49 +0800

    feat: Added new module

---
+++ src/ingest/ingest_weather_data.py
@@ -0,0 +1,9 @@
+'''
+    Data Ingestion Module to ingest
+    Daily Weather Forecast.
+'''
+def ingest_daily_weather_forecast_data(url: str) -> dict:
+    '''
+        Function to ingest daily weather forecast data.
+    '''
+    print('Hello World!')
