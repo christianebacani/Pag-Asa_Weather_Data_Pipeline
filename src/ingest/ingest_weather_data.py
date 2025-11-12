57f6dc880aa89bb848e92e33e97722bdc1fad7b4
christianebacani <christianbacani581@gmail.com>
2025-11-12 23:07:38 +0800

    Modify the content of the function 'ingest_daily_weather_forecast_data' to add conditional statements when scraping the issued datetime for the daily weather forecast and add feature to scrape the synopsis for the daily weather forecast

--- src/ingest/ingest_weather_data.py
+++ src/ingest/ingest_weather_data.py
@@ -22,7 +22,12 @@ def ingest_daily_weather_forecast_data(url: str) -> dict:
     # Get the issued datetime for the daily weather forecast    
     issued_datetime_tag = div_tag_with_row_class.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
     bold_tag = issued_datetime_tag.find('b')
-    issued_datetime = str(bold_tag.text).strip()
+
+    if bold_tag is None:
+        issued_datetime = ''
+    
+    else:
+        issued_datetime = str(bold_tag.text).strip()
 
     list_of_all_div_tag_with_col_md_twelve_col_lg_twelve_classes = div_tag_with_row_class.find_all('div', attrs={'class': 'col-md-12 col-lg-12'})
 
@@ -31,3 +36,16 @@ def ingest_daily_weather_forecast_data(url: str) -> dict:
         forecast_weather_conditions_tag = list_of_all_div_tag_with_col_md_twelve_col_lg_twelve_classes[1]
         forecast_wind_and_coastal_water_conditions_tag = list_of_all_div_tag_with_col_md_twelve_col_lg_twelve_classes[2]
         temperature_and_relative_humidity_tag = list_of_all_div_tag_with_col_md_twelve_col_lg_twelve_classes[3]
+    
+    # TODO: Implement functionality here to add else statement if the available div tags is not equals to 4 instances
+    
+    # Get the synopsis for the daily weather forecast
+    div_tag_with_panel_class = synopsis_tag.find('div', attrs={'class': 'panel'})
+    
+    if div_tag_with_panel_class is None:
+        synopsis = ''
+
+    else:
+        div_tag_with_panel_body_class = div_tag_with_panel_class.find('div', attrs={'class': 'panel-body'})
+        paragraph_tag = div_tag_with_panel_body_class.find('p')
+        synopsis = str(paragraph_tag.text).strip()
