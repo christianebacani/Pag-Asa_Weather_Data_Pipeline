e919bdb75abaea32c30febca1c5339937fc64e30
christianebacani <christianbacani581@gmail.com>
2025-11-14 12:44:56 +0800

    feat: Added content to the function 'get_forecast_wind_and_coastal_water_conditions'

--- src/ingest/daily_weather_forecast.py
+++ src/ingest/daily_weather_forecast.py
@@ -92,3 +92,33 @@ def get_forecast_wind_and_coastal_water_conditions(soup: BeautifulSoup) -> dict:
         Function to get the forecast wind and coastal water conditions
         from the daily weather forecast
     '''
+    forecast_wind_and_coastal_water_conditions = {
+        'place': [],
+        'speed': [],
+        'direction': [],
+        'coastal_water': []
+    }
+
+    forecast_wind_and_coastal_water_conditions_tag = soup.find_all('div', attrs={'class': 'col-md-12 col-lg-12'})[2]
+    table_tag = forecast_wind_and_coastal_water_conditions_tag.find('table', attrs={'class': 'table table-striped'})
+
+    if table_tag is not None:
+        tbody_tag = table_tag.find('tbody')
+        list_of_all_table_row_tags = tbody_tag.find_all('tr')
+
+        for table_row_tag in list_of_all_table_row_tags:
+            list_of_all_table_data_tags = table_row_tag.find_all('td', attrs={'class': 'text-center'})
+
+            place = str(list_of_all_table_data_tags[0].text).strip()
+            forecast_wind_and_coastal_water_conditions['place'].append(place)
+
+            speed = str(list_of_all_table_data_tags[1].text).strip()
+            forecast_wind_and_coastal_water_conditions['speed'].append(speed)
+
+            direction = str(list_of_all_table_data_tags[2].text).strip()
+            forecast_wind_and_coastal_water_conditions['direction'].append(direction)
+
+            coastal_water = str(list_of_all_table_data_tags[3].text).strip()
+            forecast_wind_and_coastal_water_conditions['coastal_water'].append(coastal_water)
+    
+    return forecast_wind_and_coastal_water_conditions
