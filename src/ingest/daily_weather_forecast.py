2ee441714fbc9685de1f38bc7599d2f8511c6237
christianebacani <christianbacani581@gmail.com>
2025-11-14 11:06:29 +0800

    feat: Added new function 'get_issued_datetime'

--- src/ingest/daily_weather_forecast.py
+++ src/ingest/daily_weather_forecast.py
@@ -4,7 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def init_soup_object(url: str) -> object | None:
+def init_soup_object(url: str) -> BeautifulSoup | None:
     '''
         Function to initialize Beautiful 
         Soup Object from the requested data
@@ -18,3 +18,18 @@ def init_soup_object(url: str) -> object | None:
 
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
+
+def get_issued_datetime(soup: BeautifulSoup) -> str:
+    '''
+        Function to get issued datetime
+        from the daily weather forecast
+    '''
+    issued_datetime = ''
+
+    issued_datetime_tag = soup.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
+    bold_tag = issued_datetime_tag.find('b')
+
+    if bold_tag is not None:
+        issued_datetime = str(bold_tag.text).strip()
+    
+    return issued_datetime    
