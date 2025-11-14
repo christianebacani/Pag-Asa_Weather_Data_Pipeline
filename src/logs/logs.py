f0c94621b9c37e3b3915fde295c3b86f32328d72
christianebacani <christianbacani581@gmail.com>
2025-11-14 10:51:35 +0800

    refact: Modify the name of the function to be executed

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -7,21 +7,22 @@ sys.path.insert(0, os.path.abspath('src'))
 
 import pandas as pd
 from datetime import datetime
-from ingest.ingest_weather_data import ingest_daily_weather_forecast_data
+from ingest.daily_weather_forecast import init_soup_object
 
 def generate_logs(log_message: str) -> None:
     '''
-        Function to generate logs based on the ETL Pipeline job/s.
+        Function to generate logs based on 
+        the ETL Pipeline job/s.
     '''
     format = '%Y-%m-%d %H:%M:%S'
     now = datetime.now()
     timestamp = now.strftime(format)
-    
+
     # Generate logs using pandas for logs dataset (csv format)
     logs = pd.read_csv('src/logs/logs.csv')
     logs = pd.concat([logs, pd.DataFrame({'messages': [log_message], 'timestamps': [timestamp]})], ignore_index=True)
     logs.to_csv('src/logs/logs.csv', index=False)
 
 if __name__ == '__main__':
-    ingest_daily_weather_forecast_data('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
+    init_soup_object('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
     generate_logs('(DEV): Ingest daily weather forecast data')
