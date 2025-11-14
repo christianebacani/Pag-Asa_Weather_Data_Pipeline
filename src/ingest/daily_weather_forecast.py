ce5d762bd6ecbf601771f962ae7ccaeb8969fd86
christianebacani <christianbacani581@gmail.com>
2025-11-14 11:50:49 +0800

    feat: Added new function 'get_forecasted_weather_conditions'

--- src/ingest/daily_weather_forecast.py
+++ src/ingest/daily_weather_forecast.py
@@ -56,3 +56,33 @@ def get_forecasted_weather_conditions(soup: BeautifulSoup) -> dict:
         Function to get the forecasted weather conditions
         from the daily weather forecast
     '''
+    forecast_weather_conditions = {
+        'place': [],
+        'weather_condition': [],
+        'caused_by': [],
+        'impacts': []
+    }
+
+    forecast_weather_conditions_tag = soup.find_all('div', attrs={'class': 'col-md-12 col-lg-12'})[1]
+    table_tag = forecast_weather_conditions_tag.find('table', attrs={'class': 'table table-striped'})
+
+    if table_tag is not None:
+        tbody_tag = table_tag.find('tbody')
+        list_of_all_table_row_tags = tbody_tag.find_all('tr')
+
+        for table_row_tag in list_of_all_table_row_tags:
+            list_of_all_table_data_tags = table_row_tag.find_all('td', attrs={'class': 'text-center'})
+            
+            place = str(list_of_all_table_data_tags[0].text).strip()
+            forecast_weather_conditions['place'].append(place)
+
+            weather_condition = str(list_of_all_table_data_tags[1].text).strip()
+            forecast_weather_conditions['weather_condition'].append(weather_condition)
+
+            caused_by = str(list_of_all_table_data_tags[2].text).strip()
+            forecast_weather_conditions['caused_by'].append(caused_by)
+
+            impacts = str(list_of_all_table_data_tags[3].text).strip()
+            forecast_weather_conditions['impacts'].append(impacts)
+    
+    return forecast_weather_conditions
