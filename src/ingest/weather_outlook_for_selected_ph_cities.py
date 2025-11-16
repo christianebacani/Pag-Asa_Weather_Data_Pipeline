d8e7777cc54f3ca98b43a0bed24e17ad6ac443d6
christianebacani <christianbacani581@gmail.com>
2025-11-16 16:59:56 +0800

    feat: Added new function 'get_oh_city_outlook_issued_datetime'

--- src/ingest/weather_outlook_for_selected_ph_cities.py
+++ src/ingest/weather_outlook_for_selected_ph_cities.py
@@ -18,3 +18,20 @@ def get_ph_city_weather_outlook_soup(url: str) -> BeautifulSoup | None:
     
     soup = BeautifulSoup(response.text, 'html.parser') # Parse to a Beautiful Soup Object
     return soup
+
+def get_ph_city_outlook_issued_datetime(soup: BeautifulSoup) -> str:
+    '''
+        Function to get issued datetime from the weather outlook 
+        for selected ph cities.
+    '''
+    issued_datetime = ''
+
+    issued_datetime_tag = soup.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
+    div_tag_with_validity_class = issued_datetime_tag.find('div', attrs={'class': 'validity'})
+
+    if div_tag_with_validity_class is not None:
+        issued_datetime = div_tag_with_validity_class.find_all('b')[0]
+        issued_datetime = str(issued_datetime.text)
+        issued_datetime = ' '.join(issued_datetime.split())
+
+    return issued_datetime
