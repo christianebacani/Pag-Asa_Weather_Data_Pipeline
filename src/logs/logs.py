8688d6dc686074903e0828c5abaf324f6b0b62fc
christianebacani <christianbacani581@gmail.com>
2025-11-19 14:59:20 +0800

    refact: Fix the parameter variable name in function 'map_weather_dates_for_selected_ph_cities'

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     ph_city_outlook_issued_datetime = get_ph_city_outlook_issued_datetime(ph_city_weather_outlook_soup)
     ph_city_outlook_valid_period = get_ph_city_outlook_valid_period(ph_city_weather_outlook_soup)
     selected_ph_cities = get_all_selected_ph_cities(ph_city_weather_outlook_soup)
-    weather_outlook_for_ph_cities = map_weather_dates_for_selected_ph_cities(get_ph_city_weather_outlook_soup, selected_ph_cities)
+    weather_outlook_for_ph_cities = map_weather_dates_for_selected_ph_cities(ph_city_weather_outlook_soup, selected_ph_cities)
     weather_outlook_for_ph_cities = map_temperature_ranges_for_selected_ph_cities(ph_city_weather_outlook_soup, weather_outlook_for_ph_cities)
     map_daily_rain_chances_to_selected_ph_cities(ph_city_weather_outlook_soup, weather_outlook_for_ph_cities)
     generate_logs('(DEV): Ingest the weather outlook for selected ph cities data.')
