36cdfcb770c899c3fa3520b7a66af55573832733
christianebacani <christianbacani581@gmail.com>
2025-11-20 11:14:03 +0800

    feat: Variable to the return value of function 'get_selected_ph_cities'

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -48,5 +48,5 @@ if __name__ == '__main__':
     ph_city_weather_outlook_soup = get_ph_city_weather_outlook_soup('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities')
     ph_city_weather_outlook_issued_datetime = get_ph_city_weather_outlook_issued_datetime(ph_city_weather_outlook_soup)
     ph_city_weather_outlook_valid_period = get_ph_city_weather_outlook_valid_period(ph_city_weather_outlook_soup)
-    get_selected_ph_cities(ph_city_weather_outlook_soup)
+    selected_ph_cities = get_selected_ph_cities(ph_city_weather_outlook_soup)
     generate_logs('(DEV): Ingest the weather outlook for selected ph cities data.')
