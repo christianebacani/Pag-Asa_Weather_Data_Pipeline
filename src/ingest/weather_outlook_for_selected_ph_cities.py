bb9076b8123710ffc9c2bf7b355898041b1a3ee7
christianebacani <christianbacani581@gmail.com>
2025-11-20 11:19:08 +0800

    feat: Comments for every function

--- src/ingest/weather_outlook_for_selected_ph_cities.py
+++ src/ingest/weather_outlook_for_selected_ph_cities.py
@@ -24,13 +24,14 @@ def get_ph_city_weather_outlook_issued_datetime(soup: BeautifulSoup) -> str:
     '''
     issued_datetime = ''
 
+    # Fetch the necessary HTML tags using find() method
     div_tag_with_row_weather_page_class = soup.find('div', attrs={'class': 'row weather-page'})
     div_tag_with_row_class = div_tag_with_row_weather_page_class.find('div', attrs={'class': 'row'})
 
     issued_datetime_and_valid_period_tag = div_tag_with_row_class.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
     div_tag_with_validity_class = issued_datetime_and_valid_period_tag.find('div', attrs={'class': 'validity'})
 
-    if div_tag_with_validity_class is not None:
+    if div_tag_with_validity_class is not None: # Validate the div_tag_with_validity_class if it's existing
         bold_tag = div_tag_with_validity_class.find_all('b')[0]
         issued_datetime = str(bold_tag.text).strip()
         issued_datetime = ' '.join(issued_datetime.split())
@@ -43,14 +44,15 @@ def get_ph_city_weather_outlook_valid_period(soup: BeautifulSoup) -> str:
         selected ph cities from pag-asa dost website.
     '''
     valid_period = ''
-
+    
+    # Fetch the necessary HTML tags using find() method
     div_tag_with_row_weather_page_class = soup.find('div', attrs={'class': 'row weather-page'})
     div_tag_with_row_class = div_tag_with_row_weather_page_class.find('div', attrs={'class': 'row'})
 
     issued_datetime_and_valid_period_tag = div_tag_with_row_class.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
     div_tag_with_validity_class = issued_datetime_and_valid_period_tag.find('div', attrs={'class': 'validity'})
 
-    if div_tag_with_validity_class is not None:
+    if div_tag_with_validity_class is not None: # Validate the div_tag_with_validity_class if it's existing
         bold_tag = div_tag_with_validity_class.find_all('b')[1]
         valid_period = str(bold_tag.text).strip()
 
@@ -62,21 +64,23 @@ def get_selected_ph_cities(soup: BeautifulSoup) -> dict[str, dict]:
         outlook from pag-asa dost website.
     '''
     result = {}
-
+    
+    # Fetch the necessary HTML tags using find() method
     div_tag_with_row_weather_page_class = soup.find('div', attrs={'class': 'row weather-page'})
     div_tag_with_row_class = div_tag_with_row_weather_page_class.find('div', attrs={'class': 'row'})
 
     selected_ph_cities_weather_outlook_tag = div_tag_with_row_class.find('div', attrs={'class': 'col-md-12 col-lg-12'})
-    div_class_with_panel_group_class = selected_ph_cities_weather_outlook_tag.find('div', attrs={'class': 'panel-group'})
+    div_tag_with_panel_group_class = selected_ph_cities_weather_outlook_tag.find('div', attrs={'class': 'panel-group'})
 
-    if div_class_with_panel_group_class is None:
+    if div_tag_with_panel_group_class is None: # Validate the div_tag_with_panel_group_class if it's missing
         return result
 
-    list_of_div_tag_with_panel_default_classes = div_class_with_panel_group_class.find_all('div', attrs={'class': 'panel panel-default panel-pagasa'})
+    list_of_div_tag_with_panel_default_classes = div_tag_with_panel_group_class.find_all('div', attrs={'class': 'panel panel-default panel-pagasa'})
 
+    # Using for-loop to iterate for every div_tag_with_panel_default_classes to fetch the necessary data
     for div_tag_with_panel_default_class in list_of_div_tag_with_panel_default_classes:
         anchor_tag = div_tag_with_panel_default_class.find('a', attrs={'data-toggle': 'collapse'})
         ph_city = str(anchor_tag.text).strip()
         result[ph_city] = {}
-    
+
     return result
