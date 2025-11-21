5404aef7125af753804b5c6cbc787269ef346033
christianebacani <christianbacani581@gmail.com>
2025-11-21 14:35:31 +0800

    Refactor the comment for the conditional statement from the function 'extract_daily_weather_forecast_soup', Create new function 'extract_daily_weather_forecast_issued_datetime'

--- src/ingest/daily_weather_forecast.py
+++ src/ingest/daily_weather_forecast.py
@@ -11,8 +11,25 @@ def extract_daily_weather_forecast_soup(url: str) -> BeautifulSoup | None:
     '''
     response = requests.get(url)
 
-    if response.status_code != 200: # Validate first if the status code of the response is not successful
+    if response.status_code != 200: # We need to check if the status code of the response for the request is unsuccessful
         return None
 
     soup = BeautifulSoup(response.text, 'html.parser') # Parse as a Beautiful Soup Object
     return soup
+
+def extract_daily_weather_forecast_issued_datetime(soup: BeautifulSoup) -> str:
+    '''
+        Function to extract the issued datetime of daily weather forecast
+        from pag-asa dost website.
+    '''
+    issued_datetime = ''
+
+    # Extract the necessary html tags to get the issued datetime of daily weather forecast
+    div_tag_with_row_weather_page_class = soup.find('div', attrs={'class': 'row weather-page'})
+    issued_datetime_tag = div_tag_with_row_weather_page_class.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
+    bold_tag = issued_datetime_tag.find('b')
+
+    if bold_tag is not None: # We need to check if the bold_tag is not missing
+        issued_datetime = str(bold_tag.text).strip()
+
+    print(issued_datetime)
