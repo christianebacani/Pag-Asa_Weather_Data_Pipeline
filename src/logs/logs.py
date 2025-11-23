f49d5921c434a02dc290bf6994d0979e09d05b3a
christianebacani <christianbacani581@gmail.com>
2025-11-23 16:58:49 +0800

    refact: Function 'ingest_data_of_daily_weather_forecast' to 'execute_functions_to_ingest_daily_weather_forecast'

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('src'))
 import pandas as pd
 from datetime import datetime
 
-from executor.daily_weather_forecast_executor import ingest_data_of_daily_weather_forecast
+from executor.daily_weather_forecast_executor import execute_functions_to_ingest_daily_weather_forecast
 
 def generate_logs(log_message: str) -> None:
     '''
@@ -29,5 +29,5 @@ def generate_logs(log_message: str) -> None:
     logs.to_csv('src/logs/logs.csv', index=False)
 
 if __name__ == '__main__':
-    ingest_data_of_daily_weather_forecast()
+    execute_functions_to_ingest_daily_weather_forecast()
     generate_logs('(DEV): Ingest the data for the daily weather forecast.')
