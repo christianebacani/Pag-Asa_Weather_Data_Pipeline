2bce0cd8e2e4298198afa3621688d4562d468a82
christianebacani <christianbacani581@gmail.com>
2025-11-24 12:15:02 +0800

    refact: Top-level comment of the function 'create_subdir'

--- src/ingest/weather_outlook_for_ph_cities.py
+++ src/ingest/weather_outlook_for_ph_cities.py
@@ -8,15 +8,10 @@ from bs4 import BeautifulSoup
 
 def create_subdir() -> None:
     '''
-        Function to create 
-        data/raw/weather_outlook_for_ph_cities/
-        subdirectory to store 
-        dedicated json files for 
-        the ingested data of 
-        weather outlook for 
-        selected philippine cities
-        from the website of 
-        pag-asa dost website.
+        Function to create data/raw/weather_outlook_for_ph_cities/
+        subdirectory to store dedicated json files
+        for the ingested data of weather outlook for selected
+        philippine cities from the website of pag-asa dost.
     '''
     # Create the data/raw/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/weather_outlook_for_ph_cities'):
