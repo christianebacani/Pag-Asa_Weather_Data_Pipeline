041b85e77dd263797fc4fa09dec15709bb537ca1
christianebacani <christianbacani581@gmail.com>
2025-11-24 11:53:33 +0800

    asd

--- src/ingest/weather_outlook_for_ph_cities.py
+++ src/ingest/weather_outlook_for_ph_cities.py
@@ -6,18 +6,23 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-def create_weather_outlook_for_ph_cities_subdir() -> None:
+def create_subdir() -> None:
     '''
-        Function to create data/raw/weather_outlook_for_ph_cities/
-        subdirectory to store dedicated json files
-        for the ingested data of weather outlook for selected philippine cities
-        from the website of pag-asa dost website.
+        Function to create 
+        data/raw/weather_outlook_for_ph_cities/
+        subdirectory to store 
+        dedicated json files for 
+        the ingested data of 
+        weather outlook for 
+        selected philippine cities
+        from the website of 
+        pag-asa dost website.
     '''
     # Create the data/raw/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/weather_outlook_for_ph_cities'):
         os.makedirs('data/raw/weather_outlook_for_ph_cities')
 
-def extract_weather_outlook_for_ph_cities_soup(url: str) -> BeautifulSoup | None:
+def extract_beautiful_soup_object(url: str) -> BeautifulSoup | None:
     '''
         Function to extract beautiful soup object of weather outlook for
         the selected philippine cities from the website of pag-asa dost.
@@ -32,14 +37,15 @@ def extract_weather_outlook_for_ph_cities_soup(url: str) -> BeautifulSoup | None
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
-def extract_weather_outlook_for_ph_cities_issued_datetime(soup: BeautifulSoup) -> str:
+def extract_issued_datetime(soup: BeautifulSoup) -> str:
     '''
-        Function to extract the issued datetime of weather outlook for selected philippine
-        cities from the website of pag-asa dost.
+        Function to extract the issued datetime of weather 
+        outlook for selected philippine cities from the 
+        website of pag-asa dost.
     '''
     issued_datetime = ''
 
-    # Extract the necessary html tags to get the issued datetime of weather outlook for selected philippine cities
+    # Extract the necessary html tags to get the issued datetime of weather outlook for the selected philippine cities
     div_tag_with_row_weather_page_class = soup.find('div', attrs={'class': 'row weather-page'})
     issued_datetime_and_valid_period_tag = div_tag_with_row_weather_page_class.find('div', attrs={'class': 'col-md-12 col-lg-12 issue'})
     div_tag_with_validity_class = issued_datetime_and_valid_period_tag.find('div', attrs={'class': 'validity'})
