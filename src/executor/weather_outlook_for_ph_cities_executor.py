62888f9b2e5961c5a1bf6fe23aebe169c5212843
christianebacani <christianbacani581@gmail.com>
2025-11-24 13:36:56 +0800

    refact: Top-level comment of the module and function

--- src/executor/weather_outlook_for_ph_cities_executor.py
+++ src/executor/weather_outlook_for_ph_cities_executor.py
@@ -1,7 +1,7 @@
 '''
     Module to execute functions for ingesting, transformation, 
-    loading, and etc. of weather outlook for selected philippine
-    cities from the website of pag-asa dost.
+    loading, and etc. of weather outlook for selected Philippine
+    cities from the PAGASA-DOST website.
 '''
 from ingest.weather_outlook_for_ph_cities import create_subdir
 from ingest.weather_outlook_for_ph_cities import extract_beautiful_soup_object
@@ -12,10 +12,10 @@ def execute_functions_to_ingest_weather_outlook_for_ph_cities() -> None:
     '''
         Function to execute all functions from the
         weather_outlook_for_ph_cities module of src/ingest/ package
-        to ingest the data of weather outlook for selected philippine
-        cities from the website of pag-asa dost.
+        to ingest the data of weather outlook for selected Philippine
+        cities from the PAGASA-DOST website.
     '''
-    # Execute all the functions to ingest the data of weather outlook for selected philippine cities from the website of pag-asa dost
+    # Execute all the functions to ingest the data of weather outlook for selected Philippine cities from the PAGASA-DOST website
     create_subdir()
     soup = extract_beautiful_soup_object('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities')
 
