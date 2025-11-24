5ce509f5e71881db62d0002f233abef1795f7666
christianebacani <christianbacani581@gmail.com>
2025-11-24 12:15:38 +0800

    refact: Function names

--- src/executor/daily_weather_forecast_executor.py
+++ src/executor/daily_weather_forecast_executor.py
@@ -3,10 +3,10 @@
     loading, and etc. of daily weather forecast from the website of
     pag-asa dost.
 '''
-from ingest.daily_weather_forecast import create_daily_weather_forecast_subdir
-from ingest.daily_weather_forecast import extract_daily_weather_forecast_soup
-from ingest.daily_weather_forecast import extract_daily_weather_forecast_issued_datetime
-from ingest.daily_weather_forecast import save_daily_forecast_issued_datetime_to_json
+from ingest.daily_weather_forecast import create_subdir
+from ingest.daily_weather_forecast import extract_beautiful_soup_object
+from ingest.daily_weather_forecast import extract_issued_datetime
+from ingest.daily_weather_forecast import save_issued_datetime_to_json
 from ingest.daily_weather_forecast import extract_synopsis
 from ingest.daily_weather_forecast import save_synopsis_to_json
 from ingest.daily_weather_forecast import extract_forecast_weather_conditions
@@ -24,20 +24,20 @@ def execute_functions_to_ingest_daily_weather_forecast() -> None:
         website of pag-asa dost.
     '''
     # Execute all the functions to ingest the data of daily weather forecast from the website of pag-asa dost
-    create_daily_weather_forecast_subdir()
-    daily_weather_forecast_soup = extract_daily_weather_forecast_soup('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
+    create_subdir()
+    soup = extract_beautiful_soup_object('https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast')
 
-    daily_weather_forecast_issued_datetime = extract_daily_weather_forecast_issued_datetime(daily_weather_forecast_soup)
-    save_daily_forecast_issued_datetime_to_json(daily_weather_forecast_issued_datetime)
+    issued_datetime = extract_issued_datetime(soup)
+    save_issued_datetime_to_json(issued_datetime)
 
-    synopsis = extract_synopsis(daily_weather_forecast_soup)
+    synopsis = extract_synopsis(soup)
     save_synopsis_to_json(synopsis)
 
-    forecast_weather_conditions = extract_forecast_weather_conditions(daily_weather_forecast_soup)
+    forecast_weather_conditions = extract_forecast_weather_conditions(soup)
     save_forecast_weather_conditions_to_json(forecast_weather_conditions)
 
-    forecast_wind_and_coastal_water_conditions = extract_forecast_wind_and_coastal_water_conditions(daily_weather_forecast_soup)
+    forecast_wind_and_coastal_water_conditions = extract_forecast_wind_and_coastal_water_conditions(soup)
     save_forecast_wind_and_coastal_water_conditions_to_json(forecast_wind_and_coastal_water_conditions)
-
-    temperature_and_relative_humidity = extract_temperature_and_relative_humidity(daily_weather_forecast_soup)
+    
+    temperature_and_relative_humidity = extract_temperature_and_relative_humidity(soup)
     save_temperature_and_relative_humidity_to_json(temperature_and_relative_humidity)
