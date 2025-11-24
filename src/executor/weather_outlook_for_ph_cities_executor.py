7d148d0389fa24f3798c58c8996f051ebd7e8612
christianebacani <christianbacani581@gmail.com>
2025-11-24 12:49:08 +0800

    feat: Function 'save_issued_datetime_to_json'

--- src/executor/weather_outlook_for_ph_cities_executor.py
+++ src/executor/weather_outlook_for_ph_cities_executor.py
@@ -6,6 +6,7 @@
 from ingest.weather_outlook_for_ph_cities import create_subdir
 from ingest.weather_outlook_for_ph_cities import extract_beautiful_soup_object
 from ingest.weather_outlook_for_ph_cities import extract_issued_datetime
+from ingest.weather_outlook_for_ph_cities import save_issued_datetime_to_json
 
 def execute_functions_to_ingest_weather_outlook_for_ph_cities() -> None:
     '''
@@ -19,3 +20,4 @@ def execute_functions_to_ingest_weather_outlook_for_ph_cities() -> None:
     soup = extract_beautiful_soup_object('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities')
 
     issued_datetime = extract_issued_datetime(soup)
+    save_issued_datetime_to_json(issued_datetime)
