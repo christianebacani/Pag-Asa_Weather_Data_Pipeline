833c97ac9b469f8dba6a987954b5e88e6618f775
christianebacani <christianbacani581@gmail.com>
2025-11-26 14:39:16 +0800

    feat: Comment and function 'ingest_weather_outlook_for_ph_tourist_areas'

--- src/executor/weather_outlook_for_ph_tourist_areas_executor.py
+++ src/executor/weather_outlook_for_ph_tourist_areas_executor.py
@@ -4,6 +4,7 @@
     tourist areas from the PAGASA-DOST website.
 '''
 from ingest.weather_outlook_for_ph_tourist_areas import create_subdir
+from ingest.weather_outlook_for_ph_tourist_areas import extract_beautiful_soup_object
 
 def ingest_weather_outlook_for_ph_tourist_areas() -> None:
     '''
@@ -11,4 +12,6 @@ def ingest_weather_outlook_for_ph_tourist_areas() -> None:
         areas from the PAGASA-DOST website by executing all functions from
         weather_outlook_for_ph_tourist_areas module of src/ingest/ package.
     '''
+    # Execute all the functions to ingest the data of weather outlook for selected Philippine tourist areas from the PAGASA-DOST website
     create_subdir()
+    ingest_weather_outlook_for_ph_tourist_areas()
