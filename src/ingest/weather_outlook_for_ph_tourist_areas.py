900009cd0ec1a80a757b25be83410cd275ebf4da
christianebacani <christianbacani581@gmail.com>
2025-11-26 14:26:31 +0800

    feat: Function 'extract_beautiful_soup_object'

--- src/ingest/weather_outlook_for_ph_tourist_areas.py
+++ src/ingest/weather_outlook_for_ph_tourist_areas.py
@@ -3,6 +3,7 @@
     selected Philippine tourist areas from the PAGASA-DOST website.
 '''
 import os
+from bs4 import BeautifulSoup
 
 def create_subdir() -> None:
     '''
@@ -14,3 +15,15 @@ def create_subdir() -> None:
     # Create the data/raw/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/weather_outlook_for_ph_tourist_areas'):
         os.makedirs('data/raw/weather_outlook_for_ph_tourist_areas')
+
+def extract_beautiful_soup_object(url: str) -> BeautifulSoup:
+    '''
+    Function to extract the BeautifulSoup object of weather
+    outlook for selected Philippine cities from the PAGASA-DOST 
+    website.
+
+    :param url: Url of the PAGASA-DOST website that consist of weather outlook for selected Philippine tourist areas
+    :type url: str
+    :return:
+    :rtype: BeautifulSoup
+    '''
