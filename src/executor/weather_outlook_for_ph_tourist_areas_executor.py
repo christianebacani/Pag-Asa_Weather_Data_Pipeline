b16a6eb3fa34e43575f4f0fb1bf90efaf89507fc
christianebacani <christianbacani581@gmail.com>
2025-11-26 14:19:51 +0800

    refact: Function name and docstring

--- src/executor/weather_outlook_for_ph_tourist_areas_executor.py
+++ src/executor/weather_outlook_for_ph_tourist_areas_executor.py
@@ -5,11 +5,10 @@
 '''
 from ingest.weather_outlook_for_ph_tourist_areas import create_subdir
 
-def execute_functions_to_ingest_weather_outlook_for_ph_tourist_areas() -> None:
+def ingest_weather_outlook_for_ph_tourist_areas() -> None:
     '''
-        Function to execute all functions from the
-        weather_outlook_for_ph_tourist_areas module of src/ingest/ package
-        to ingest the data of weather outlook for selected Philippine
-        tourist areas from the PAGASA-DOST website.
+        Function to ingest weather outlook for selected Philippine tourist
+        areas from the PAGASA-DOST website by executing all functions from
+        weather_outlook_for_ph_tourist_areas module of src/ingest/ package.
     '''
     create_subdir()
