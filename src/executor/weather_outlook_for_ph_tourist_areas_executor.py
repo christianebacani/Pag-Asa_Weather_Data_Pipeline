c11c5e933bbbf702113328fb0ef7656252d9b6d8
christianebacani <christianbacani581@gmail.com>
2025-11-27 13:25:53 +0800

    refact: Structure for function 'ingest_weather_outlook_for_ph_tourist_areas' when initializing it, and the structure of the content of that docstring

--- src/executor/weather_outlook_for_ph_tourist_areas_executor.py
+++ src/executor/weather_outlook_for_ph_tourist_areas_executor.py
@@ -9,16 +9,21 @@ from ingest.weather_outlook_for_ph_tourist_areas import extract_issued_datetime
 from ingest.weather_outlook_for_ph_tourist_areas import save_issued_datetime_to_json
 from ingest.weather_outlook_for_ph_tourist_areas import extract_valid_period
 
-def ingest_weather_outlook_for_ph_tourist_areas() -> None:
+def ingest_weather_outlook_for_ph_tourist_areas(
+) -> None:
     '''
-        Function to ingest weather outlook for selected Philippine tourist
-        areas from the PAGASA-DOST website by executing all functions from
-        weather_outlook_for_ph_tourist_areas module of src/ingest/ package.
+        Function to ingest weather outlook for selected
+        Philippine tourist areas from the PAGASA-DOST
+        website by executing all functions from
+        weather_outlook_for_ph_tourist_areas module of
+        src/ingest/ package.
     '''
     # Execute all the functions to ingest the data of weather outlook for selected 
     # Philippine tourist areas from the PAGASA-DOST website
     create_subdir()
-    soup = extract_beautiful_soup_object('https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas')
+    soup = extract_beautiful_soup_object(
+        'https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas'
+    )
 
     issued_datetime = extract_issued_datetime(soup)
     save_issued_datetime_to_json(issued_datetime)
