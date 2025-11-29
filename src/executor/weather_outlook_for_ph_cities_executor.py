0db06568f0103f01d05102997f275a816f8cef11
christianebacani <christianbacani581@gmail.com>
2025-11-29 14:39:41 +0800

    refact: Delete unnecessary functions

--- src/executor/weather_outlook_for_ph_cities_executor.py
+++ src/executor/weather_outlook_for_ph_cities_executor.py
@@ -12,10 +12,6 @@ from ingest.weather_outlook_for_ph_cities import extract_valid_period
 from ingest.weather_outlook_for_ph_cities import save_valid_period_to_json
 from ingest.weather_outlook_for_ph_cities import extract_ph_city_tags
 from ingest.weather_outlook_for_ph_cities import extract_ph_city_names
-from ingest.weather_outlook_for_ph_cities import map_weather_dates_to_ph_cities
-from ingest.weather_outlook_for_ph_cities import map_temperature_ranges_to_ph_cities
-from ingest.weather_outlook_for_ph_cities import map_chances_of_rain_pct_to_ph_cities
-from ingest.weather_outlook_for_ph_cities import save_ph_cities_weather_outlook_to_json
 
 def ingest_weather_outlook_for_ph_cities(
 ) -> None:
@@ -41,16 +37,4 @@ def ingest_weather_outlook_for_ph_cities(
     list_of_all_ph_city_tags = extract_ph_city_tags(soup)
 
     ph_city_names = extract_ph_city_names(list_of_all_ph_city_tags)
-    ph_cities_with_weather_dates = map_weather_dates_to_ph_cities(
-        list_of_all_ph_city_tags,
-        ph_city_names
-    )
-    ph_cities_weather_outlook = map_temperature_ranges_to_ph_cities(
-        list_of_all_ph_city_tags,
-        ph_cities_with_weather_dates
-    )
-    ph_cities_weather_outlook = map_chances_of_rain_pct_to_ph_cities(
-        list_of_all_ph_city_tags,
-        ph_cities_weather_outlook
-    )
-    save_ph_cities_weather_outlook_to_json(ph_cities_weather_outlook)
+    
