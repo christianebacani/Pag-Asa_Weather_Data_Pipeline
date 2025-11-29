91a7fdeebc88cf349adccb75039ded65a76dc185
christianebacani <christianbacani581@gmail.com>
2025-11-29 15:36:45 +0800

    fix: Parameter when executing the function 'extract_temperature_ranges'

--- src/executor/weather_outlook_for_ph_cities_executor.py
+++ src/executor/weather_outlook_for_ph_cities_executor.py
@@ -43,4 +43,4 @@ def ingest_weather_outlook_for_ph_cities(
     weather_dates = extract_weather_dates(soup)
     ph_cities_with_weather_dates = map_weather_dates_to_ph_cities(weather_dates, ph_city_names)
 
-    extract_temperature_ranges(soup)
+    extract_temperature_ranges(list_of_all_ph_city_tags)
