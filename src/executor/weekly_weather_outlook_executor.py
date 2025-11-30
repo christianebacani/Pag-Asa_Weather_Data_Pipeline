6e0982c7324e300bcbac59d6e7a26665bd88e633
christianebacani <christianbacani581@gmail.com>
2025-11-30 17:19:58 +0800

    feat: New function 'ingest_weekly_weather_outlook'

--- src/executor/weekly_weather_outlook_executor.py
+++ src/executor/weekly_weather_outlook_executor.py
@@ -3,3 +3,12 @@
     and loading functions for the weekly weather
     outlook from the PAGASA-DOST website.
 '''
+def ingest_weekly_weather_outlook(
+) -> None:
+    '''
+        Ingests the weekly weather outlook
+        from the PAGASA-DOST website by
+        executing all functions in the
+        weekly_weather_outlook module of
+        the src/ingest package..
+    '''
