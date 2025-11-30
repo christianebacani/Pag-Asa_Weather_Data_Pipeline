784f2adae46a42d1641db2f190b0014896592228
christianebacani <christianbacani581@gmail.com>
2025-11-30 17:15:04 +0800

    feat: Imported modules, and new function 'create_subdir'

--- src/ingest/weekly_weather_outlook.py
+++ src/ingest/weekly_weather_outlook.py
@@ -2,3 +2,19 @@
     Module to ingest weekly weather outlook
     from the PAGASA-DOST website.
 '''
+import os
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def create_subdir(
+) -> None:
+    '''
+        Creates the data/raw/weekly_weather_outlook/
+        subdirectory to store JSON files for weekly
+        weather outlook data ingested from the PAGASA-
+        DOST website.
+    '''
+    # Create the data/raw/weekly_weather_outlook/ subdirectory if it doesn't exist
+    if not os.path.exists('data/raw/weekly_weather_outlook'):
+        os.makedirs('data/raw/weekly_weather_outlook')
