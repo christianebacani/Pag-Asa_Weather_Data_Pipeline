457e03b80f669574e2f5dc3a8eb481f8b10d0371
christianebacani <christianbacani581@gmail.com>
2025-11-30 17:27:29 +0800

    feat: New function 'extract_beautiful_soup_object

--- src/ingest/weekly_weather_outlook.py
+++ src/ingest/weekly_weather_outlook.py
@@ -18,3 +18,30 @@ def create_subdir(
     # Create the data/raw/weekly_weather_outlook/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/weekly_weather_outlook'):
         os.makedirs('data/raw/weekly_weather_outlook')
+    
+def extract_beautiful_soup_object(
+        url: str
+) -> BeautifulSoup | None:
+    '''
+    Extracts the BeautifulSoup object of the
+    weekly weather outlook page from the
+    PAGASA-DOST website.
+
+    :param url: URL of the PAGASA-DOST page
+    containing the weekly weather outlook
+    :type url: str
+
+    :return: BeautifulSoup object for navigating
+    and manipulating the page content, or None if
+    extraction fails
+    :rtype: BeautifulSoup | None
+    '''
+    response = requests.get(url)
+
+    # We need to check if the status code of the response for the request is unsuccessful
+    if response.status_code != 200:
+        return None
+    
+    # Parse as a BeautifulSoup object
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
