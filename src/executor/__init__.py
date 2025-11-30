1f6223f478be417a38163b3ce62fe440fd9f69d7
christianebacani <christianbacani581@gmail.com>
2025-11-30 17:21:27 +0800

    feat: Modules to initialize

--- src/executor/__init__.py
+++ src/executor/__init__.py
@@ -1,1 +1,4 @@
 from . import daily_weather_forecast_executor
+from . import weather_outlook_for_ph_cities_executor
+from . import weather_outlook_for_ph_tourist_areas_executor
+from . import weekly_weather_outlook_executor
