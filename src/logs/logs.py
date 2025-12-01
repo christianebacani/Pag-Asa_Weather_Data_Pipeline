9ce70205937b62cb8a30fd3bc008729541898d60
christianebacani <christianbacani581@gmail.com>
2025-12-01 14:19:14 +0800

    feat: New function 'ingest_daily_temperature'

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -14,6 +14,7 @@ from executor.daily_weather_forecast_executor import ingest_daily_weather_foreca
 from executor.weather_outlook_for_ph_cities_executor import ingest_weather_outlook_for_ph_cities
 from executor.weather_outlook_for_ph_tourist_areas_executor import ingest_weather_outlook_for_ph_tourist_areas
 from executor.weekly_weather_outlook_executor import ingest_weekly_weather_outlook
+from executor.daily_temperature_executor import ingest_daily_temperature
 
 def generate_logs(
     log_message: str
@@ -65,4 +66,10 @@ if __name__ == '__main__':
     ingest_weekly_weather_outlook()
     generate_logs(
         '(DEV): Ingest the data for the weekly weather outlook.'
+    )
+
+    # Ingest data for daily temperature
+    ingest_daily_temperature()
+    generate_logs(
+        '(DEV): Ingest the data for the daily temperature'
     )
