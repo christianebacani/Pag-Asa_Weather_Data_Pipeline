55bee3cc189a03fdacea624d1ce8367aaf2f8e4f
christianebacani <christianbacani581@gmail.com>
2025-12-01 14:14:13 +0800

    feat: New module 'daily_temperature_executor.py' and function 'ingest_daily_temperature'

---
+++ src/executor/daily_temperature_executor.py
@@ -0,0 +1,16 @@
+'''
+    Module for executing ingestion, transformation,
+    and loading functions for the daily temperature
+    including top 10 lowest and top 10 highest recored
+    across different weather stations from the PAGASA-DOST
+    website.
+'''
+def ingest_daily_temperature(
+) -> None:
+    '''
+        Ingests the daily temperature
+        from the PAGASA-DOST website by
+        executing all functions in the
+        daily_temperature module of
+        src/ingest package.
+    '''
