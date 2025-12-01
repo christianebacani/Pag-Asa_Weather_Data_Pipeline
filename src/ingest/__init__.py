4226c77e0140cbe9c348ea432e279ec013414a6f
christianebacani <christianbacani581@gmail.com>
2025-12-01 14:06:32 +0800

    feat: Initialized module 'daily_temperature.py'

--- src/ingest/__init__.py
+++ src/ingest/__init__.py
@@ -2,3 +2,4 @@ from . import daily_weather_forecast
 from . import weather_outlook_for_ph_cities
 from . import weather_outlook_for_ph_tourist_areas
 from . import weekly_weather_outlook
+from . import daily_temperature
