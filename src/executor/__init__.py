0b14333cd3f27cd6a958fbdea9a84ba5681a427a
christianebacani <christianbacani581@gmail.com>
2025-12-04 13:03:43 +0800

    feat: Added new module

--- src/executor/__init__.py
+++ src/executor/__init__.py
@@ -3,3 +3,4 @@ from . import weather_outlook_for_ph_cities_executor
 from . import weather_outlook_for_ph_tourist_areas_executor
 from . import weather_advisory_executor
 from . import tropical_cyclone_advisory_executor
+from . import tropical_cyclone_associated_rainfall_executor
