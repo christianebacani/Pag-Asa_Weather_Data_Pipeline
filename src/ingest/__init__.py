387a77ad31f0d1a09595c80f3c3bd070048d0801
christianebacani <christianbacani581@gmail.com>
2025-12-04 13:04:41 +0800

    feat: New Module

--- src/ingest/__init__.py
+++ src/ingest/__init__.py
@@ -3,3 +3,4 @@ from . import weather_outlook_for_ph_cities
 from . import weather_outlook_for_ph_tourist_areas
 from . import weather_advisory
 from . import tropical_cyclone_advisory
+from . import tropical_cyclone_associated_rainfall
