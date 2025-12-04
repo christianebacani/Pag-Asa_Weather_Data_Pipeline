a439060abe5ed1ca4a8942c87ba6b8745a23f2fc
christianebacani <christianbacani581@gmail.com>
2025-12-04 13:05:01 +0800

    Added new logs

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -15,6 +15,7 @@ from executor.weather_outlook_for_ph_cities_executor import ingest_weather_outlo
 from executor.weather_outlook_for_ph_tourist_areas_executor import ingest_weather_outlook_for_ph_tourist_areas
 from executor.weather_advisory_executor import ingest_weather_advisory
 from executor.tropical_cyclone_advisory_executor import ingest_tropical_cyclone_advisory
+from executor.tropical_cyclone_associated_rainfall_executor import ingest_tropical_cyclone_associated_rainfall
 
 def generate_logs(
     log_message: str
@@ -72,4 +73,10 @@ if __name__ == '__main__':
     ingest_tropical_cyclone_advisory()
     generate_logs(
         '(DEV): Ingests the data for the tropical cyclone advisory'
+    )
+
+    # Ingest data for the tropical cyclone associated rainfall
+    ingest_tropical_cyclone_associated_rainfall()
+    generate_logs(
+        '(DEV): Ingest the data for the tropical cyclone associated rainfall'
     )
