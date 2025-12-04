81a4c11da1fea90212e8c18ccd696cba1a96c016
christianebacani <christianbacani581@gmail.com>
2025-12-04 12:55:59 +0800

    feat: New module 'tropical_cyclone_advisory_executor.py' with funct 'ingest_tropical_cyclone_advisory'

---
+++ src/executor/tropical_cyclone_advisory_executor.py
@@ -0,0 +1,15 @@
+'''
+    Module for executing ingestion, transformation,
+    and loading functions for the tropical cyclone
+    advisory from the PAGASA-DOST website.
+'''
+
+def ingest_tropical_cyclone_advisory(
+) -> None:
+    '''
+        Ingest the tropical cyclone
+        advisory from the PAGASA-DOST
+        website by executing all functions
+        in the weather_advisory module
+        of the src/ingest package.
+    '''
