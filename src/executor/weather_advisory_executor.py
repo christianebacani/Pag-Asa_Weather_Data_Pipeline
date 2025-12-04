b99c55aa06d4caca6a284cb257d65d5ba6c752c0
christianebacani <christianbacani581@gmail.com>
2025-12-04 12:16:10 +0800

    feat: Module 'weather_advisory_executor.py' and funct 'ingest_weather_advisory'

---
+++ src/executor/weather_advisory_executor.py
@@ -0,0 +1,15 @@
+'''
+    Module for executing ingestion, transformation,
+    and loading functions for the weather advisory
+    from the PAGASA-DOST website.
+'''
+
+def ingest_weather_advisory(
+) -> None:
+    '''
+        Ingests the weather advisory
+        from the PAGASA-DOST website
+        by executing all functions in
+        the weather_advisory module
+        of the src/ingest package.
+    '''
