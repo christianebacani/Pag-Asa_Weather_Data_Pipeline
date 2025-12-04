e06a80bff282d1f209d882134863086f0f3e8648
christianebacani <christianbacani581@gmail.com>
2025-12-04 12:11:24 +0800

    New modules and content inside the funct 'create_subdir'

--- src/ingest/weather_advisory.py
+++ src/ingest/weather_advisory.py
@@ -2,6 +2,11 @@
     Module to ingest weather advisory from the
     PAGASA-DOST website.
 '''
+import os
+import requests
+import json
+from bs4 import BeautifulSoup
+
 def create_subdir(
 ) -> None:
     '''
@@ -9,3 +14,6 @@ def create_subdir(
         subdirectory to store JSON files for weather
         advisory from the PAGASA-DOST website.
     '''
+    # Create the data/raw/weather_advisory/ subdirectory if it doesn't exist
+    if not os.path.exists('data/raw/weather_advisory'):
+        os.makedirs('data/raw/weather_advisory')
