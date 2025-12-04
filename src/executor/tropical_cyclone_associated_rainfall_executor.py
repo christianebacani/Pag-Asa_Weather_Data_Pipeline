5ef0fc338d876f38deaba9a66fa065542e3f3637
christianebacani <christianbacani581@gmail.com>
2025-12-04 13:04:22 +0800

    feat: New module with 'ingest_tropical_cyclone_associated_rainfall' func

---
+++ src/executor/tropical_cyclone_associated_rainfall_executor.py
@@ -0,0 +1,16 @@
+'''
+    Module for executing ingestion, transformation,
+    and loading functions for the tropical cyclone
+    associated rainfall from the PAGASA-DOST website.
+'''
+
+def ingest_tropical_cyclone_associated_rainfall(
+) -> None:
+    '''
+        Ingest the tropical cyclone
+        associated rainfall from the
+        PAGASA-DOST website by executing
+        all functions in the 
+        tropical_cyclone_associated_rainfall
+        module of the src/ingest package.
+    '''
