0c170833833d5577026ce7d2b0f78d302c274fbb
christianebacani <christianbacani581@gmail.com>
2025-12-04 12:07:46 +0800

    feat: Module 'weather_advisotry' and funct 'create_subdir'

---
+++ src/ingest/weather_advisory.py
@@ -0,0 +1,11 @@
+'''
+    Module to ingest weather advisory from the
+    PAGASA-DOST website.
+'''
+def create_subdir(
+) -> None:
+    '''
+        Creates the data/raw/weather_advisory/
+        subdirectory to store JSON files for weather
+        advisory from the PAGASA-DOST website.
+    '''
