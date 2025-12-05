1ceadcd7eac39d58a21c946832b7c974fb55d158
christianebacani <christianbacani581@gmail.com>
2025-12-05 16:59:16 +0800

    feat: New module 'regional_forecast_executor.py'

---
+++ src/executor/regional_forecast_executor.py
@@ -0,0 +1,6 @@
+'''
+    Module for executing ingestion, transformation,
+    and loading functions for the regional forecast
+    specifically for the Province of Bataan and Tarlac
+    from the PAGASA-DOST website.
+'''
