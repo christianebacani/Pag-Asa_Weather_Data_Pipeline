936e56aa34131d092de6b17e324dab15feadcce5
christianebacani <christianbacani581@gmail.com>
2025-12-05 16:00:54 +0800

    feat: New funct 'create_subdir' and libraries to import (os, requests, json, BeautifulSoup)

--- src/ingest/tropical_cyclone_associated_rainfall.py
+++ src/ingest/tropical_cyclone_associated_rainfall.py
@@ -3,3 +3,16 @@
     associated rainfall from the PAGASA-
     DOST website.
 '''
+import os
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def create_subdir():
+    '''
+        Creates the
+        data/raw/tropical_cyclone_associated_rainfall/
+        subdirectory to store JSON files for
+        tropical cyclone associated rainfall from the
+        PAGASA-DOST website.
+    '''
