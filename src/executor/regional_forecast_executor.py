4f4814f9ee6296bdcbc1e155a54daa17432447df
christianebacani <christianbacani581@gmail.com>
2025-12-05 17:07:25 +0800

    fix: Docstring content of the module and the funct 'ingest_regional_forecast'

--- src/executor/regional_forecast_executor.py
+++ src/executor/regional_forecast_executor.py
@@ -1,7 +1,7 @@
 '''
     Module for executing ingestion, transformation,
     and loading functions for the regional forecast
-    specifically for the Province of Bataan and Tarlac
+    specifically for the Province of Tarlac and Bataan
     from the PAGASA-DOST website.
 '''
 def ingest_regional_forecast(
@@ -9,9 +9,8 @@ def ingest_regional_forecast(
     '''
         Ingest the regional forecast
         specifically for the Province
-        of Bataan and Tarlac
-        from the PAGASA-DOST website by
-        executing all functions in the
-        regional_forecast module of the
-        src/ingest package.
+        of Tarlac and Bataan from the
+        PAGASA-DOST website by executing
+        all functions in the regional_forecast
+        module of the src/ingest package.
     '''
