f833a7d2a8caf22cdb5761d2ff8a28f24954953e
christianebacani <christianbacani581@gmail.com>
2025-12-05 16:09:32 +0800

    feat: New funct 'extract_beautiful_soup_object'

--- src/ingest/tropical_cyclone_associated_rainfall.py
+++ src/ingest/tropical_cyclone_associated_rainfall.py
@@ -19,3 +19,22 @@ def create_subdir():
     # Create the data/raw/tropical_cyclone_associated_rainfall/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/tropical_cyclone_associated_rainfall'):
         os.makedirs('data/raw/tropical_cyclone_associated_rainfall')
+
+def extract_beautiful_soup_object(
+        url: str
+) -> BeautifulSoup | None:
+    '''
+    Extracts the BeautifulSoup object
+    of the tropical cyclone associated
+    rainfall from the PAGASA-DOST website.
+
+    :param url: URL of the PAGASA-DOST
+        page containing the tropical
+        cyclone associated rainfall
+    :type url: str
+
+    :return: BeautifulSoup object for navigating
+        the page content, or None if extraction
+        fails
+    :rtype: BeautifulSoup | None
+    '''
