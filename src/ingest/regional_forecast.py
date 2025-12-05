7a7be111c8454342c860dfbc5a5999cdf054b242
christianebacani <christianbacani581@gmail.com>
2025-12-05 17:20:22 +0800

    feat: Funct 'extract_beautiful_soup_object'

--- src/ingest/regional_forecast.py
+++ src/ingest/regional_forecast.py
@@ -21,3 +21,23 @@ def create_subdir(
     # Create the data/raw/regional_forecast/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/regional_forecast'):
         os.makedirs('data/raw/regional_forecast')
+
+def extract_beautiful_soup_object(
+        url: str
+) -> BeautifulSoup | None:
+    '''
+    Extracts the BeautifulSoup object
+    of the regional forecast specifically
+    for the Province of Tarlac and Bataan
+    from the PAGASA-DOST website.
+
+    :param url: URL of the PAGASA-DOST
+        page containing the regional
+        forecast
+    :type url: str
+
+    :return: BeautifulSoup object for navigating
+        the page content, or None if extraction
+        fails
+    :rtype: BeautifulSoup | None
+    '''
