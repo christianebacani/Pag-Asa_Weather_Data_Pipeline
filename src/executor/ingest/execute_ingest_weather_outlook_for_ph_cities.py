157e7e4b5aa3a90abd720ad00a1a8b0acc17cb31
christianebacani <christianbacani581@gmail.com>
2025-12-06 14:29:08 +0800

    feat: New module 'execute_ingest_weather_outlook_for_ph_cities.py'

---
+++ src/executor/ingest/execute_ingest_weather_outlook_for_ph_cities.py
@@ -0,0 +1,64 @@
+'''
+    Module for executing ingestion fucntions for the
+    weather outlook of selected Philippine cities from
+    the PAGASA-DOST website.
+'''
+from ingest.ingest_weather_outlook_for_ph_cities import create_subdir
+from ingest.ingest_weather_outlook_for_ph_cities import extract_beautiful_soup_object
+from ingest.ingest_weather_outlook_for_ph_cities import extract_issued_datetime
+from ingest.ingest_weather_outlook_for_ph_cities import save_issued_datetime_to_json
+from ingest.ingest_weather_outlook_for_ph_cities import extract_valid_period
+from ingest.ingest_weather_outlook_for_ph_cities import save_valid_period_to_json
+from ingest.ingest_weather_outlook_for_ph_cities import extract_ph_city_tags
+from ingest.ingest_weather_outlook_for_ph_cities import extract_ph_city_names
+from ingest.ingest_weather_outlook_for_ph_cities import extract_weather_dates
+from ingest.ingest_weather_outlook_for_ph_cities import map_weather_dates_to_ph_cities
+from ingest.ingest_weather_outlook_for_ph_cities import extract_temperature_ranges
+from ingest.ingest_weather_outlook_for_ph_cities import map_temperature_ranges_to_ph_cities
+from ingest.ingest_weather_outlook_for_ph_cities import extract_chance_of_rain_percentages
+from ingest.ingest_weather_outlook_for_ph_cities import map_chance_of_rain_percentages_to_ph_cities
+from ingest.ingest_weather_outlook_for_ph_cities import save_ph_cities_weather_outlook_to_json
+
+def ingest_weather_outlook_for_ph_cities(
+) -> None:
+    '''
+        Ingests the weather outlook for selected
+        Philippine cities from the PAGASA-DOST
+        website by executing all functions in the
+        ingest_weather_outlook_for_ph_cities module
+        of the src/ingest package.
+    '''
+    # Run all functions to ingest weather outlook data for selected Philippine cities
+    create_subdir()
+    soup = extract_beautiful_soup_object(
+        'https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-philippine-cities'
+    )
+
+    issued_datetime = extract_issued_datetime(soup)
+    save_issued_datetime_to_json(issued_datetime)
+
+    valid_period = extract_valid_period(soup)
+    save_valid_period_to_json(valid_period)
+
+    list_of_all_ph_city_tags = extract_ph_city_tags(soup)
+    ph_city_names = extract_ph_city_names(list_of_all_ph_city_tags)
+
+    weather_dates = extract_weather_dates(list_of_all_ph_city_tags)
+    ph_cities_with_weather_dates = map_weather_dates_to_ph_cities(
+        weather_dates,
+        ph_city_names
+    )
+
+    temperature_ranges = extract_temperature_ranges(list_of_all_ph_city_tags)
+    ph_cities_weather_outlook = map_temperature_ranges_to_ph_cities(
+        temperature_ranges,
+        ph_cities_with_weather_dates
+    )
+
+    chance_of_rain_percentages = extract_chance_of_rain_percentages(list_of_all_ph_city_tags)
+    ph_cities_weather_outlook = map_chance_of_rain_percentages_to_ph_cities(
+        chance_of_rain_percentages,
+        ph_cities_weather_outlook
+    )
+
+    save_ph_cities_weather_outlook_to_json(ph_cities_weather_outlook)
