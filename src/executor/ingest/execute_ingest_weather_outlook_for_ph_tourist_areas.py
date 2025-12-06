3abe023b366dee4beba95a8791e928b2c0d9d192
christianebacani <christianbacani581@gmail.com>
2025-12-06 14:38:06 +0800

    feat: New module 'execute_ingest_weather_outlook_for_ph_tourist_areas.py'

---
+++ src/executor/ingest/execute_ingest_weather_outlook_for_ph_tourist_areas.py
@@ -0,0 +1,56 @@
+'''
+    Module for executing ingestion functions for the
+    weather outlook of selected Philippine tourist
+    areas from the PAGASA-DOST website.
+'''
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import create_subdir
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_beautiful_soup_object
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_issued_datetime
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import save_issued_datetime_to_json
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_valid_period
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import save_valid_period_to_json
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_ph_tourist_area_tags
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_ph_tourist_area_names
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_weather_dates
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import map_weather_dates_to_ph_tourist_areas
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import extract_temperature_ranges
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import map_temperature_ranges_to_ph_tourist_areas
+from ingest.ingest_weather_outlook_for_ph_tourist_areas import save_ph_tourist_areas_weather_outlook_to_json
+
+def ingest_weather_outlook_for_ph_tourist_areas(
+) -> None:
+    '''
+        Ingests the weather outlook for selected
+        Philippine tourist areas from the PAGASA-DOST
+        website by executing all functions in the
+        ingest_weather_outlook_for_ph_tourist_areas module of
+        the src/ingest package.
+    '''
+    # Run all functions to ingest weather outlook data for selected Philippine tourist areas
+    create_subdir()
+    soup = extract_beautiful_soup_object(
+        'https://www.pagasa.dost.gov.ph/weather/weather-outlook-selected-tourist-areas'
+    )
+
+    issued_datetime = extract_issued_datetime(soup)
+    save_issued_datetime_to_json(issued_datetime)
+
+    valid_period = extract_valid_period(soup)
+    save_valid_period_to_json(valid_period)
+
+    list_of_all_ph_tourist_area_tags = extract_ph_tourist_area_tags(soup)
+    ph_tourist_area_names = extract_ph_tourist_area_names(list_of_all_ph_tourist_area_tags)
+
+    weather_dates = extract_weather_dates(soup)
+    ph_tourist_areas_with_weather_dates = map_weather_dates_to_ph_tourist_areas(
+        weather_dates,
+        ph_tourist_area_names
+    )
+
+    temperature_ranges = extract_temperature_ranges(list_of_all_ph_tourist_area_tags)
+    ph_tourist_areas_weather_outlook = map_temperature_ranges_to_ph_tourist_areas(
+        temperature_ranges,
+        ph_tourist_areas_with_weather_dates
+    )
+
+    save_ph_tourist_areas_weather_outlook_to_json(ph_tourist_areas_weather_outlook)
