45630bd997bc805265988e65a8e8a74d5230ec17
christianebacani <christianbacani581@gmail.com>
2025-12-06 13:56:20 +0800

    feat: Function 'stage_issued_datetime'

--- src/etl/extract/extract_daily_weather_forecast.py
+++ src/etl/extract/extract_daily_weather_forecast.py
@@ -2,3 +2,20 @@
     Module to extract daily weather forecast data
     from the data/raw subdirectory on the local machine.
 '''
+import pandas as pd
+
+def stage_issued_datetime(
+        issued_datetime_filepath: str
+) -> None:
+    '''
+    Stages the JSON file that contains
+    the issued datetime of the daily weather
+    forecast into the data/stage subdirectory
+    on the local machine.
+
+    :param issued_datetime_filepath: Relative
+        filepath of the JSON file that contains
+        the issued datetime of the daily weather
+        forecast
+    :type issued_datetime_filepath: str
+    '''
