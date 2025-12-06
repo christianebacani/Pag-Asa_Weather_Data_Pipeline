05d46ca236210ff788dfd29dd9cda16bda7f0687
christianebacani <christianbacani581@gmail.com>
2025-12-06 14:39:06 +0800

    feat: New module 'execute_ingest_weather_advisory.py'

---
+++ src/executor/ingest/execute_ingest_weather_advisory.py
@@ -0,0 +1,27 @@
+'''
+    Module for executing ingestion functions for
+    the weather advisory from the PAGASA-DOST
+    website.
+'''
+from ingest.ingest_weather_advisory import create_subdir
+from ingest.ingest_weather_advisory import extract_beautiful_soup_object
+from ingest.ingest_weather_advisory import extract_weather_advisory
+from ingest.ingest_weather_advisory import save_weather_advisory_to_json
+
+def ingest_weather_advisory(
+) -> None:
+    '''
+        Ingests the weather advisory
+        from the PAGASA-DOST website
+        by executing all functions in
+        the ingest_weather_advisory module
+        of the src/ingest package.
+    '''
+    # Run all functions to ingest weather advisory data
+    create_subdir()
+    soup = extract_beautiful_soup_object(
+        'https://www.pagasa.dost.gov.ph/weather/weather-advisory'
+    )
+
+    weather_advisory = extract_weather_advisory(soup)
+    save_weather_advisory_to_json(weather_advisory)
